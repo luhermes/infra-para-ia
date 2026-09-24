b44540bb00b625e79d23fcc32da185efaf429315
Luana Barbara <hermes.lu@icloud.com>
2026-09-24 02:33:56 +0000

    Troca tamanho do node por falta de capacidade na regiao

--- aula4-iac/variables.tf
+++ aula4-iac/variables.tf
@@ -46,7 +46,7 @@ variable "memoria" {
 variable "node_vm_size" {
   description = "Tamanho da VM do nó do AKS."
   type        = string
-  default     = "Standard_D2as_v7"
+  default     = "Standard_D2ads_v7"
 }
 
 variable "node_count" {
